e26b93a22ce3c3c2d475b3238dabad28ac002a70
kenypospi <keny.pospi@seznam.cz>
2026-08-17 14:48:08 +0200

    Read product visibility from current Google sheet

--- nastaveni.js
+++ nastaveni.js
@@ -1,5 +1,5 @@
 // HONZŮV MARKET – Google Tabulky
-window.HONZUV_MARKET_PRODUKTY_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRa_jvuLVK2rXb6tXZZKPcpCEj4CqCWlXCdayHCqo-Fg2CW84FFmTzgr35jmLsF1Q/pub?gid=443885828&single=true&output=csv";
+window.HONZUV_MARKET_PRODUKTY_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRa_jvuLVK2rXb6tXZZKPcpCEj4CqCWlXCdayHCqo-Fg2CW84FFmTzgr35jmLsF1Q/pub?gid=1307362107&single=true&output=csv";
 window.HONZUV_MARKET_KATEGORIE_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRa_jvuLVK2rXb6tXZZKPcpCEj4CqCWlXCdayHCqo-Fg2CW84FFmTzgr35jmLsF1Q/pub?gid=47548890&single=true&output=csv";
 window.HONZUV_MARKET_NASTAVENI_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRa_jvuLVK2rXb6tXZZKPcpCEj4CqCWlXCdayHCqo-Fg2CW84FFmTzgr35jmLsF1Q/pub?gid=221722833&single=true&output=csv";
 window.HONZUV_MARKET_SEKCE_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRa_jvuLVK2rXb6tXZZKPcpCEj4CqCWlXCdayHCqo-Fg2CW84FFmTzgr35jmLsF1Q/pub?gid=560558408&single=true&output=csv";
