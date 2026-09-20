7d1c220a42f00dca1952612c22ba7fe70b518031
kenypospi <keny.pospi@seznam.cz>
2026-09-20 22:50:26 +0200

    Aktivovat cenu produktu 618043

--- ceny.js
+++ ceny.js
@@ -1,1 +1,1 @@
-window.HONZUV_MARKET_CENY = {};
+window.HONZUV_MARKET_CENY = {,"618043":{"cena":223.89,"prodejniJednotka":"ks","akce":false,"vyprodej":false,"zobrazit":true,"nejprodavanejsi":false,"novinka":false,"doporucujeme":false}};
